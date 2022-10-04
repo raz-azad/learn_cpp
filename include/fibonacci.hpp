729d6a484556804e53a088a68e44386beff10c9c
Rashed Azad <rashed.azad@neura-robotics.com>
2022-10-04 13:49:22 +0200

    raz : changed cmake

--- include/fibonacci.hpp
+++ include/fibonacci.hpp
@@ -5,8 +5,8 @@
 class Fibonacci
 {
 private:
-	std::map<int,unsigned long long int> store_fib_result_;
+	std::map<int, unsigned long long int> store_fib_result_;
 
 public:
-	unsigned long long getFib(const int& fib);
+	unsigned long long getFib(const int &fib);
 };
--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -1,10 +1,9 @@
-
 #include <fibonacci.hpp>
 
-unsigned long long Fibonacci::getFib(const int& fib)
+unsigned long long Fibonacci::getFib(const int &fib)
 {
 	auto it = store_fib_result_.find(fib);
-	if(it != store_fib_result_.end())
+	if (it != store_fib_result_.end())
 	{
 		return it->second;
 	}
@@ -16,11 +15,12 @@ unsigned long long Fibonacci::getFib(const int& fib)
 			store_fib_result_[fib] = fib;
 			return store_fib_result_[fib];
 		default:
-			auto result1 = getFib(fib-1);
-			store_fib_result_[fib-1] = result1;
+			auto result1 = getFib(fib - 1);
+			store_fib_result_[fib - 1] = result1;
 
-			store_fib_result_[fib-2] = getFib(fib-2);
-			store_fib_result_[fib] = store_fib_result_[fib-1] + store_fib_result_[fib-2];
+			store_fib_result_[fib - 2] = getFib(fib - 2);
+			store_fib_result_[fib] = store_fib_result_[fib - 1]
+					+ store_fib_result_[fib - 2];
 			return store_fib_result_[fib];
 		}
 	}
